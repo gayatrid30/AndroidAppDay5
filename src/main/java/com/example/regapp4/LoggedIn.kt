f23bbc328fbe96d530a8cad8d7190021878e3b36
Gayatri D <61691060+gayatrid30@users.noreply.github.com>
2020-06-07 13:31:55 +0530

    Add files via upload

---
+++ src/main/java/com/example/regapp4/LoggedIn.kt
@@ -0,0 +1,20 @@
+package com.example.regapp4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class LoggedIn : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logged_in)
+
+
+    }
+}
